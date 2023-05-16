377ba14c63adccd41e0be998a4f9de1a11f95301
Md Jahid Hasan <jahid.facts@gmail.com>
2023-05-16 21:54:35 +0600

    ok

--- js/script.js
+++ js/script.js
@@ -87,16 +87,3 @@ function portfolioItemDetails(portfolioItem) {
 
 
 
-
-
-// actie menu 
-var header = document.getElementById("nav");
-var btns = header.getElementsByClassName("menu_item");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
-
